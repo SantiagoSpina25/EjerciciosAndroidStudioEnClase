201bae8ae208999be924638cc1267c60d2c9df94
Santiago Medac <ssp0017@alu.medac.es>
2024-10-15 14:39:34 +0200

    15-10

--- unSoloFragment/app/src/main/java/com/example/unsolofragment/BlankFragment.java
+++ unSoloFragment/app/src/main/java/com/example/unsolofragment/BlankFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,19 +47,19 @@ public class BlankFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View view = inflater.inflate(R.layout.fragment_blank,container, false);
+        Button btn1 = view.findViewById(R.id.btn2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().remove(BlankFragment.this).commit();
+            }
+        });
+
+        return view;
     }
 }
