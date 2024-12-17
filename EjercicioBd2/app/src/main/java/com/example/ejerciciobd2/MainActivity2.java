252f05d9a22dc5d60c535f8b1dafad8cc4f36c4d
Santiago Medac <ssp0017@alu.medac.es>
2024-12-17 08:57:11 +0100

    ejercicios nuevos

--- EjercicioBd2/app/src/main/java/com/example/ejerciciobd2/MainActivity2.java
+++ EjercicioBd2/app/src/main/java/com/example/ejerciciobd2/MainActivity2.java
@@ -1,7 +1,10 @@
 package com.example.ejerciciobd2;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,5 +22,31 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         tvDatos = findViewById(R.id.tvDatos);
+        DbHelper dbHelper = new DbHelper(MainActivity2.this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        String texto = "";
+        if(dbHelper != null){
+            Cursor cursor = db.rawQuery("SELECT * FROM t_contactos",null);
+
+            while(cursor.moveToNext()){
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String apellido = cursor.getString(2);
+                String telefono = cursor.getString(3);
+                String email = cursor.getString(4);
+                String direccion = cursor.getString(5);
+
+                texto = texto + "\n Id: " + id + "\n Nombre: " + name + "\n Apellido: " + apellido +"\n Telefono:"+ telefono + "\n Email: " + email + "\n Direccion: " + direccion + "\n \n";
+            }
+            tvDatos.setText(texto);
+            cursor.close();
+
+        }else{
+            Toast.makeText(MainActivity2.this,"Error al mostrar la base de datos", Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 }
